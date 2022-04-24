0035c846934115e80951f168ca0ede5241e7af01
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-04-24 21:30:29 +0100

    room-homepage

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500;600;700&display=swap');
+body{
+  font-family: 'League Spartan', sans-serif;
+}
+
+</style>
